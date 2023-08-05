d9b6a8efd0c58b8d984d1f508c55f7aeb757b2e8
Nith567 <91722732+Nith567@users.noreply.github.com>
2023-08-05 11:13:19 +0530

    Update Post.js

--- src/app/models/Post.js
+++ src/app/models/Post.js
@@ -28,4 +28,5 @@ const postSchema = new Schema(
   { timestamps: true }
 );
 
-export const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
+ const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
+export default Post;
